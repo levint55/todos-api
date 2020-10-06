966aa44bdafff136c6839544d7af89c68fc47bc2
amabel <levintabel@gmail.com>
2020-10-06 19:46:21 +0700

    Add DELETE for todos

--- server.js
+++ server.js
@@ -46,6 +46,19 @@ app.post("/todos", (req, res) => {
   res.json(body);
 });
 
+app.delete("/todos/:id", (req, res) => {
+  var todoId = parseInt(req.params.id, 10);
+  var matchedTodo = _.findWhere(todos, {id: todoId});
+
+  if (!matchedTodo){
+    res.status(404).json({"error": "no todo found with that id"});
+  } else {
+    todos = _.without(todos, matchedTodo);
+  }
+
+  res.json(todos);
+});
+
 app.listen(port, () => {
   console.log("Express running todo API");
 });
