01b56c7d71c720e7d2f5e0318aa24699a8f1607c
amabel <levintabel@gmail.com>
2020-10-08 15:28:15 +0700

    Basic Sqlite

--- playground/basic-sqlite-database.js
+++ playground/basic-sqlite-database.js
@@ -7,20 +7,53 @@ var sequelize = new Sequelize(undefined, undefined, undefined, {
 var Todo = sequelize.define("todo", {
   description: {
     type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true,
+      len: [1, 250],
+    },
   },
   completed: {
     type: Sequelize.BOOLEAN,
+    allowNull: false,
+    defaultValue: false,
   },
 });
 
 sequelize.sync().then(() => {
   console.log("Everything is synched");
 
-  Todo.create({
-    description: "Walk",
-    completed: true,
-  }).then((todo) => {
-    console.log("Create todo");
-    console.log(todo);
-  });
+  Todo.findById(1)
+    .then((todo) => {
+      console.log(todo.toJSON());
+    })
+    .catch((e) => {
+      console.log(e);
+    });
+
+  // Todo.create({
+  //   description: "Test Lagi",
+  // }).then(() => {
+  //   return Todo.create({
+  //     description: "Clean"
+  //   });
+  // }).then(() => {
+  //   return Todo.findAll({
+  //     where: {
+  //       description: {
+  //         $like: '%lean%'
+  //       }
+  //     }
+  //   })
+  // }).then((todos) => {
+  //   if(todos){
+  //     todos.forEach(todo => {
+  //       console.log(todo.toJSON());
+  //     });
+  //   } else {
+  //     console.log("No data found");
+  //   }
+  // }).catch((e) => {
+  //   console.log(e);
+  // });
 });
